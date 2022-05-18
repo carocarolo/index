112cab09fa9da7c036e8d018750c0b6f279dab81
carocarolo <103548449+carocarolo@users.noreply.github.com>
2022-05-18 19:00:26 -0400

    Update prueba_app.py

--- prueba_app.py
+++ prueba_app.py
@@ -4,10 +4,10 @@ import pandas as pd
 import altair as alt
 
 
-header = st.beta_container()
-dataset=st.beta_container()
-features=st.beta_container()
-modelTraining=st.beta_container()
+header = st.container()
+dataset=st.container()
+features=st.container()
+modelTraining=st.container()
 
 with header: 
   st.title('Welcome to my life project')
@@ -19,7 +19,7 @@ with dataset:
   data=pd.read_csv('zara.csv')
   st.write(data.head(20))
   
-  
+  st.subheader('This is a subheader')
   chart_data = pd.DataFrame(data)
   st.write(chart_data.head(20))
   bar_chart=alt.Chart(chart_data).mark_bar().encode(
@@ -32,8 +32,15 @@ with dataset:
 with features:
   st.title('Welcome to my life project')
   st.text('In this project I look into transactions')
+  st.markdown('* **first feature:**I created this ')
+  st.markdown('* **second feature:**I created this ')
   
 with modelTraining:
   st.title('Welcome to my life project')
   st.text('In this project I look into transactions')
+  
+  sel_col,disp_col=st.betacolumns(2)
+  max_depth=sel_col.slider('What is the max_depth', min_value=10,max_value=100,value=20,step=10)
+  n_estimators=sel_col.selectbox('How many trees should there be?',options=(100,200,300,'No limit'), index=
+  input_feature=sel_col.text_input('Which feature should be used','PULocationID')
   
