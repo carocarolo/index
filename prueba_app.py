59246c490fc5229375d31e98bc097a63ba05a96c
carocarolo <103548449+carocarolo@users.noreply.github.com>
2022-05-18 18:00:06 -0400

    Update prueba_app.py

--- prueba_app.py
+++ prueba_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 header = st.beta_container()
 dataset=st.beta_container()
@@ -12,6 +13,7 @@ with header:
 with dataset: 
   st.title('Welcome to my life project')
   st.text('In this project I look into transactions')
+  data=pd.read_csv('C:/Users/Carolina Mendoza/Documents/Applied Analytics/Proyectos/zara.csv')
   
 with features:
   st.title('Welcome to my life project')
