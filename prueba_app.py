c64a259550212897cc22690ac49a26e690db9c17
carocarolo <103548449+carocarolo@users.noreply.github.com>
2022-05-18 21:14:44 -0400

    Update prueba_app.py

--- prueba_app.py
+++ prueba_app.py
@@ -43,22 +43,34 @@ with all_countries:
 with top10:
   st.title('Countries where Zara is more expensive')
   chart_top10 = chart_data.nlargest(10,'price_USD')
-  bar_chart=alt.Chart(chart_top10).mark_bar().encode(
+  bar_chart_top=alt.Chart(chart_top10).mark_bar().encode(
     y=alt.Y('country_name:O',sort='-x'),
     x='price_USD:Q',
   )
-  text = bar_chart.mark_text(
+  text = bar_chart_top.mark_text(
     align='left',
     baseline='middle',
     dx=3  # Nudges text to right so it doesn't appear on top of the bar
 ).encode(
     text='price_USD:Q'
 )
-  st.altair_chart(bar_chart, use_container_width=True)
+  st.altair_chart(bar_chart_top, use_container_width=True)
   
   
 with bottom10:
   st.title('Countries where Zara is cheaper')
-  chart_top10 = chart_data.nsmallest(10,'price_USD')
+  chart_bottom10 = chart_data.nsmallest(10,'price_USD')
+  bar_chart_bottom=alt.Chart(chart_bottom10).mark_bar().encode(
+    y=alt.Y('country_name:O',sort='-x'),
+    x='price_USD:Q',
+  )
+  text = bar_chart_bottom.mark_text(
+    align='left',
+    baseline='middle',
+    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+).encode(
+    text='price_USD:Q'
+)
+  st.altair_chart(bar_chart_bottom, use_container_width=True)
   
   
