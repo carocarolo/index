562984db34774bfdf864b70f34f94080f8ca4798
carocarolo <103548449+carocarolo@users.noreply.github.com>
2022-05-18 21:59:19 -0400

    Update news_app.py

--- news_app.py
+++ news_app.py
@@ -2,18 +2,24 @@ import requests
 import pandas as pd
 import json
 import streamlit as st
-import pycountry
+
+ticker=st.selectbox('Select ticker',options=(AAPL,EC), index=0)
+btn=st.button('Buscar')
 
 #Establishing URL parameters
-fmt = 'https://api.twelvedata.com/time_series?symbol={ticker}&interval={interval}&apikey={apikey}&source=docs'
-values = { 'apikey': 'demo', 'ticker': 'AAPL','interval':'1day' }
-url=fmt.format(**values)
-#print(url)
+if btn:
+  fmt = f'https://api.twelvedata.com/time_series?symbol={ticker}&interval=1day&apikey=demo&source=docs'
 
 #Retrieving data from Website
 r = requests.get(url)
 result = r.json()
-print (type(result).__name__)
-print(result)
 
+aapl = result['values']
+pt=pd.DataFrame(aapl)
+
+
+#Retrieve News Data 
+fmt_news = f'https://eodhistoricaldata.com/api/news?api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX&s={ticker}'
+r_news = requests.get(url)
+result_news = r_news.json()
 
