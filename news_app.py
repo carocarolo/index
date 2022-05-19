4eada8a64ecee498837b8d850ea38138e643e912
carocarolo <103548449+carocarolo@users.noreply.github.com>
2022-05-18 21:31:05 -0400

    Update news_app.py

--- news_app.py
+++ news_app.py
@@ -1,4 +1,19 @@
 import requests
 import pandas as pd
 import json
+import streamlit as st
+import pycountry
+
+#Establishing URL parameters
+fmt = 'https://api.twelvedata.com/time_series?symbol={ticker}&interval={interval}&apikey={apikey}&source=docs'
+values = { 'apikey': 'demo', 'ticker': 'AAPL','interval':'1day' }
+url=fmt.format(**values)
+#print(url)
+
+#Retrieving data from Website
+r = requests.get(url)
+result = r.json()
+print (type(result).__name__)
+print(result)
+
 
