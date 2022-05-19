b2ffc5f4646565b7828d62e4a18ee3b9cdcaa409
carocarolo <103548449+carocarolo@users.noreply.github.com>
2022-05-18 21:12:25 -0400

    Update prueba_app.py

--- prueba_app.py
+++ prueba_app.py
@@ -14,14 +14,13 @@ bottom10=st.container()
 
 st.sidebar.title("Welcome Streamlitters!")
 
+
 with header: 
   st.title('The Zara Index')
   st.text('The Zara Index is a proxy to estimate in what countries fashion is more affordable and understand the difference of prices across different countries ')
   st.subheader('How was this index born?')
   st.text('This index was born on a trip to Venice')
 
-with sidebar:
-    my_component(greeting="hello")
   
 with all_countries: 
   st.subheader('How much is this dress in every country of the world?')
@@ -42,17 +41,24 @@ with all_countries:
   
   
 with top10:
-  st.title('Welcome to my life project')
-  st.text('In this project I look into transactions')
-  st.markdown('* **first feature:**I created this ')
-  st.markdown('* **second feature:**I created this ')
+  st.title('Countries where Zara is more expensive')
+  chart_top10 = chart_data.nlargest(10,'price_USD')
+  bar_chart=alt.Chart(chart_top10).mark_bar().encode(
+    y=alt.Y('country_name:O',sort='-x'),
+    x='price_USD:Q',
+  )
+  text = bar_chart.mark_text(
+    align='left',
+    baseline='middle',
+    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+).encode(
+    text='price_USD:Q'
+)
+  st.altair_chart(bar_chart, use_container_width=True)
+  
   
 with bottom10:
-  st.title('Welcome to my life project')
-  st.text('In this project I look into transactions')
+  st.title('Countries where Zara is cheaper')
+  chart_top10 = chart_data.nsmallest(10,'price_USD')
   
-  sel_col,disp_col=st.beta_columns(2)
-  max_depth=sel_col.slider('What is the max_depth', min_value=10,max_value=100,value=20,step=10)
-  n_estimators=sel_col.selectbox('How many trees should there be?',options=(100,200,300,'No limit'), index=0)
-  input_feature=sel_col.text_input('Which feature should be used','PULocationID')
   
